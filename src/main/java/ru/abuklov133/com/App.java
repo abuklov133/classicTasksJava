10cff7225173b25b8cc8cf11b9ed7688df413598
abuklov133 <abuklov133@gmail.com>
2024-02-17 09:44:35 +0300

    Start repository classicTasksJava

---
+++ src/main/java/ru/abuklov133/com/App.java
@@ -0,0 +1,13 @@
+package ru.abuklov133.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
